54bcd12a4bce09a4b171054a30d7dcdec93dff28
Baron-burton <brandon_burton159@hotmail.com>
2015-12-26 22:32:34 +0000

    Beginning to add User Authentication
    ensuring uniqueness of emails taken by database

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+describe User do
+	let(:valid_attributes) {
+		{
+			first_name: "Brandon",
+			last_name: "Burton",
+			email: "brandon@gmail.com"
+		}
+	}
+  context "validations" do
+  	let(:user) { User.new(valid_attributes) }
+
+    before do
+      User.create(valid_attributes)
+    end
+
+  	it "requires an email" do
+  		expect(user).to validate_presence_of(:email)
+  end
+
+    it "requires a unique email" do 
+      expect(user).to validate_uniqueness_of(:email)
+  end
+
+    it "requies a unique email (case insensitive)" do 
+      user.email = "BURTON@GMAIL.COM"
+      expect(user).to validate_uniqueness_of(:email)
+  end
+
+    
+end
+
+
+  context "#downcase_email" do
+    it "makes the email attribute lower case" do
+      user = User.new(valid_attributes.merge(email: "BURTON@GMAIL.COM"))
+      expect { user.downcase_email }.to change { user.email }.
+      from("BURTON@GMAIL.COM").
+      to("burton@gmail.com")
+    end
+
+    it "downcases an email before saving" do
+      user = User.new(valid_attributes)
+      user.email = "BRANDON@GMAIL.COM"
+      expect(user.save).to be_true
+      expect(user.email).to eq("brandon@gmail.com")
+    end
+  end
+end
